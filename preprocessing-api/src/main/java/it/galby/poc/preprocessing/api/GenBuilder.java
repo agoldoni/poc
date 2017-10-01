beed8922613e6f38e59e221933757eeac553036c
Aberto Goldoni <alberto.goldoni@gmail.com>
2017-10-01 17:26:38 +0200

    aaa

---
+++ preprocessing-api/src/main/java/it/galby/poc/preprocessing/api/GenBuilder.java
@@ -0,0 +1,9 @@
+package it.galby.poc.preprocessing.api;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Target;
+
+@Target(value = {ElementType.TYPE})
+public @interface GenBuilder {
+
+}
---
+++ preprocessing-test/src/main/java/it/galby/poc/preprocessing/test/Person.java
@@ -0,0 +1,10 @@
+package it.galby.poc.preprocessing.test;
+
+import it.galby.poc.preprocessing.api.GenBuilder;
+
+@GenBuilder
+public class Person {
+
+	
+	
+}
