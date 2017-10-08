b16ed88481a9ee93deb1818f9713e04258d831c0
Aberto Goldoni <alberto.goldoni@gmail.com>
2017-10-08 17:51:44 +0200

    .

---
+++ preprocessing-test/src/main/java/it/galby/poc/preprocessing/test/Main.java
@@ -0,0 +1,12 @@
+package it.galby.poc.preprocessing.test;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		Person p = new Person();
+		PersonBuilder p2 = new PersonBuilder();
+
+	}
+
+}
